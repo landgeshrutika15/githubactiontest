d1ed93eacd911464e6c8002787a134fb70581797
Landge Shrutika <landgeshrutika047@gmail.com>
2025-08-04 10:26:27 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
-public Class Hello
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello Java");
+public Class Hello{
+	public static void main(String[] args){
+	 System.out.println("Hello Java");
 	}
 }
